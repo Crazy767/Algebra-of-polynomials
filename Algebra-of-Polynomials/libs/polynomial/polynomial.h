00e7d7476f20c5491ff085402b3b9d878912d1cb
Crazy767 <sascha.scotin2011@list.ru>
2023-04-28 11:25:18 +0300

    Refresh lib

---
+++ Algebra-of-Polynomials/libs/polynomial/polynomial.h
@@ -0,0 +1,36 @@
+﻿#ifndef POLYNOMIAL_H
+#define POLYNOMIAL_H
+#include <vector>
+#include <string>
+#include <fstream>
+#include <json.hpp>
+
+using json = nlohmann::json;
+
+class Polynomial {
+public:
+    Polynomial();
+
+    Polynomial(std::vector<std::vector<std::vector<double>>> coeffs);
+
+    Polynomial operator+(const Polynomial& other) const;
+
+    Polynomial operator-(const Polynomial& other) const;
+
+    Polynomial operator*(double scalar) const;
+
+    std::string toString() const;
+
+    double operator()(double x, double y, double z) const;
+
+    void saveToFile(const std::string& filename) const;
+private:
+ 
+ 
+    std::vector<std::vector<std::vector<double>>> m_coeffs;
+};
+
+#endif
+
+
+
