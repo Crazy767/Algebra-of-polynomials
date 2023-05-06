f52c28ba0cfe47776242b22bfe75ade03751aff4
AndrewStiks <slipfis@gmail.com>
2023-05-06 18:51:20 +0300

    Gen_Interface and Linear_table_list v1.0 added

---
+++ Algebra-of-Polynomials/libs/Linear_table_list/Linear_able_list/Linear_table_list.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#ifndef LINEAR_TABLE_H
+#define LINEAR_TABLE_H
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+class LinearTable {
+public:
+    LinearTable();  // Конструктор по умолчанию
+    ~LinearTable(); // Деструктор
+    void display() const;             
+private:
+    Node* head;   // Указатель на начало списка
+    int size;     // Размер таблицы
+
+    Node* createNode(int value);      // Создание нового узла
+    void deleteNode(Node* node);      // Удаление узла
+};
+
+#endif
